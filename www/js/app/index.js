f0e9a64e6284ba48b34ef93094d2c80541e61f9b
uikoo9 <uikoo9@qq.com>
2016-09-13 12:18:38 +0800

    图文笑话以及切换

--- www/js/app/index.js
+++ www/js/app/index.js
@@ -1,68 +1,108 @@
-// app
+// vars
+var $$ = Dom7;
 var myApp = new Framework7({
     modalTitle: '笑笑',
-    material: true
+    material: true,
+    onAjaxStart: function(){
+    	myApp.showIndicator();
+    },
+    onAjaxComplete: function(){
+    	myApp.hideIndicator();
+    }
 });
-
-// Expose Internal DOM library
-var $$ = Dom7;
-
-// Add main view
 var mainView = myApp.addView('.view-main', {});
 
-// Show/hide preloader for remote ajax loaded pages
-// Probably should be removed on a production/local app
-$$(document).on('ajaxStart', function (e) {
-    if (e.detail.xhr.requestUrl.indexOf('autocomplete-languages.json') >= 0) {
-        // Don't show preloader for autocomplete demo requests
-        return;
-    }
-    myApp.showIndicator();
-});
-$$(document).on('ajaxComplete', function (e) {
-    if (e.detail.xhr.requestUrl.indexOf('autocomplete-languages.json') >= 0) {
-        // Don't show preloader for autocomplete demo requests
-        return;
-    }
-    myApp.hideIndicator();
-});
-
-/* ===== Change statusbar bg when panel opened/closed ===== */
+// binds
 $$('.panel-left').on('open', function () {
     $$('.statusbar-overlay').addClass('with-panel-left');
 });
-$$('.panel-right').on('open', function () {
-    $$('.statusbar-overlay').addClass('with-panel-right');
-});
-$$('.panel-left, .panel-right').on('close', function () {
+$$('.panel-left').on('close', function () {
     $$('.statusbar-overlay').removeClass('with-panel-left with-panel-right');
 });
-
-/* ===== Infinite Scroll Page ===== */
-myApp.onPageInit('infinite-scroll', function (page) {
-    // Loading trigger
-    var loading = false;
-    // Last loaded index, we need to pass it to script
-    var lastLoadedIndex = $$('.infinite-scroll .card').length;
-    // Attach 'infinite' event handler
-    $$('.infinite-scroll').on('infinite', function () {
-        // Exit, if loading in progress
-        if (loading) return;
-        // Set loading trigger
-        loading = true;
-        // Request some file with data
-        $$.get('infinite-scroll-load.php', {leftIndex: lastLoadedIndex + 1}, function (data) {
-            loading = false;
-            if (data === '') {
-                // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
-                myApp.detachInfiniteScroll($$('.infinite-scroll'));
-            }
-            else {
-                // Append loaded elements to list block
-                $$('.infinite-scroll .list-block ul').append(data);
-                // Update last loaded index
-                lastLoadedIndex = $$('.infinite-scroll .list-block li').length;
-            }
-        });
-    });
+$$('#toimg').on('click', function(){
+	infiniteurl = infiniteurl2;
+	infinitereset();
 });
+$$('#totxt').on('click', function(){
+	infiniteurl = infiniteurl1;
+	infinitereset();
+});
+
+// infinite scroll
+var infinitepage 	= 1;
+var infinitepagesize= 20;
+var infinitekey		= 'abaf6784cfbed433873c724ebe5f6d5e';
+var infiniteloading = false;
+var infiniteurl		= 'http://japi.juhe.cn/joke/content/text.from';
+var infiniteurl1	= 'http://japi.juhe.cn/joke/content/text.from';
+var infiniteurl2	= 'http://japi.juhe.cn/joke/img/text.from';
+$$('.infinite-scroll').on('infinite', infiniteit);
+infinitereset();
+
+// infinite reset
+function infinitereset(){
+	$$('.infinite-scroll').html('');
+	infinitepage = 1;
+	infiniteit();
+}
+
+// infinite it
+function infiniteit(){
+	if(infiniteloading) return;
+	infiniteloading = true;
+	myApp.showIndicator();
+	
+	$$.get(infiniteurl, {
+		key : infinitekey,
+		page : infinitepage,
+		pagesize : infinitepagesize
+	}, function(data) {
+		infiniteloading = false;
+		myApp.hideIndicator();
+		
+		if(data){
+			var json = JSON.parse(data);
+			if(json && json['error_code'] == 0){
+				infinitepage++;
+				
+				var ss = [];
+				for(var i=0; i<json.result.data.length; i++){
+					ss.push(infiniterow(json.result.data[i]));
+				}
+				
+				$$('.infinite-scroll').append(ss.join(''));
+				
+				console.log(ss.join(''));
+			}else{
+				myApp.detachInfiniteScroll($$('.infinite-scroll'));
+			}
+		}
+	});
+}
+
+// infinite row
+function infiniterow(item){
+	var ss = [];
+
+	ss.push('<div class="card ks-card-header-pic">');
+	if(item.url){
+		ss.push('	<div class="card-header color-white no-border lazy" valign="bottom" data-background="' + item.url + '">');
+		ss.push('		' + item.content);
+		ss.push('	</div>');
+		ss.push('	<div class="card-content">');
+		ss.push('		<div class="card-content-inner">');
+		ss.push('			<p class="color-gray">' + item.updatetime + '</p>');
+		ss.push('		</div>');
+		ss.push('	</div>');
+	}else{
+		ss.push('	<div class="card-content">');
+		ss.push('		<div class="card-content-inner">');
+		ss.push('			<p class="color-gray">' + item.updatetime + '</p>');
+		ss.push('			<p>' + item.content + '</p>');
+		ss.push('		</div>');
+		ss.push('	</div>');
+	}
+	ss.push('</div>');
+	
+	return ss.join('');
+}
